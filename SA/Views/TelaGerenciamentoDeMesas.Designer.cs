565de7d88eac1ad48ded49bc5d46ad717eb64a51
Felipe-cloud-ui <67599692+Felipe-cloud-ui@users.noreply.github.com>
2020-11-05 10:58:27 -0300

    1.54

--- SA/Views/TelaGerenciamentoDeMesas.Designer.cs
+++ SA/Views/TelaGerenciamentoDeMesas.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 127);
+            this.button1.Location = new System.Drawing.Point(157, 102);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -43,18 +44,28 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(328, 134);
+            this.button2.Location = new System.Drawing.Point(311, 102);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 23);
             this.button2.TabIndex = 0;
             this.button2.Text = "GRANDE MERDA";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 53);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // TelaGerenciamentoDeMesas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(527, 294);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "TelaGerenciamentoDeMesas";
@@ -68,5 +79,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
