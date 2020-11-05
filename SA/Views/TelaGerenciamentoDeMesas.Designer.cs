3aea1af694782dd6ed7f786b10fe6029ded56d48
JoaoVitorSchmidt <joao_v_schmidt@estudante.sc.senai.br>
2020-11-05 10:42:38 -0300

    1.51

--- SA/Views/TelaGerenciamentoDeMesas.Designer.cs
+++ SA/Views/TelaGerenciamentoDeMesas.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MERDA";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // TelaGerenciamentoDeMesas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(527, 294);
+            this.Controls.Add(this.button1);
             this.Name = "TelaGerenciamentoDeMesas";
             this.ShowIcon = false;
             this.Text = "Gerenciamento de Mesas";
@@ -43,5 +54,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
